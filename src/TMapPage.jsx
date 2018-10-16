3e1135bf596082c7df98d46d101ba6090b804ab8
Yoseob Kim <yo1226@gmail.com>
2018-10-16 13:50:11 +0000

    Details of navigation added

--- src/TMapPage.jsx
+++ src/TMapPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import {Toolbar, ToolbarButton, Page, Button, BackButton, Icon, ProgressCircular} from 'react-onsenui';
+import {Toolbar, ToolbarButton, Page, Button, BackButton, Icon, ProgressCircular, List, ListItem, ListHeader} from 'react-onsenui';
 
 import postscribe from 'postscribe';
 
@@ -9,7 +9,8 @@ export default class TmapView extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      loaded: false
+      loaded: false,
+      paths: []
     };
   }
 
@@ -30,7 +31,7 @@ export default class TmapView extends React.Component {
       "appKey=a53a0843-aa54-43c3-b8e0-e8202a107d2b><\/script>"
     ];
 
-    var this_ = this;
+    let this_ = this;
     for(let i = 0; i < scripts.length; i++) {
       postscribe('#tmapscript', scripts[i], {
         done: function() {
@@ -43,43 +44,45 @@ export default class TmapView extends React.Component {
   }
   
   loadMap() {
+    let this_ = this;
+
     let map = new Tmap.Map({
       div : 'map_div', // map을 표시해줄 div
       width : "100%", // map의 width 설정
-      height : "100%", // map의 height 설정
+      height : "50%", // map의 height 설정
     });
-    var tmapLatLng = JSON.parse(localStorage.getItem("tmapLatLng"));
+    let tmapLatLng = JSON.parse(localStorage.getItem("tmapLatLng"));
            
     map.setCenter(new Tmap.LonLat(
       "126.9850380932383", "37.566567545861645").transform("EPSG:4326", "EPSG:3857"), 15);
        //설정한 좌표를 "EPSG:3857"로 좌표변환한 좌표값으로 즁심점으로 설정합니다.
      
-    var routeLayer = new Tmap.Layer.Vector("route");//벡터 레이어 생성
-    var markerLayer = new Tmap.Layer.Markers("start");// 마커 레이어 생성
+    let routeLayer = new Tmap.Layer.Vector("route");//벡터 레이어 생성
+    let markerLayer = new Tmap.Layer.Markers("start");// 마커 레이어 생성
     map.addLayer(routeLayer);//map에 벡터 레이어 추가
     map.addLayer(markerLayer);//map에 마커 레이어 추가
   
     // 시작
-    var size = new Tmap.Size(24, 38);//아이콘 크기 설정
-    var offset = new Tmap.Pixel(-(size.w / 2), -size.h);//아이콘 중심점 설정
-    var icon = new Tmap.IconHtml(
+    let size = new Tmap.Size(24, 38);//아이콘 크기 설정
+    let offset = new Tmap.Pixel(-(size.w / 2), -size.h);//아이콘 중심점 설정
+    let icon1 = new Tmap.IconHtml(
       '<img src=http://tmapapis.sktelecom.com/upload/tmap/marker/pin_r_m_s.png />', size, offset)
     //마커 아이콘 설정
-    var marker_s = new Tmap.Marker(new Tmap.LonLat(
-      "126.9850380932383", "37.566567545861645").transform("EPSG:4326", "EPSG:3857"), icon);
+    let marker_s = new Tmap.Marker(new Tmap.LonLat(
+      "126.9850380932383", "37.566567545861645").transform("EPSG:4326", "EPSG:3857"), icon1);
     //설정한 좌표를 "EPSG:3857"로 좌표변환한 좌표값으로 설정합니다.
     markerLayer.addMarker(marker_s);//마커 레이어에 마커 추가
   
     // 도착
-    var icon = new Tmap.IconHtml(
+    let icon2 = new Tmap.IconHtml(
       '<img src=http://tmapapis.sktelecom.com/upload/tmap/marker/pin_r_m_e.png />', size, offset);
     //마커 아이콘 설정
-    var marker_e = new Tmap.Marker(new Tmap.LonLat(
-      "127.10331814639885", "37.403049076341794").transform("EPSG:4326", "EPSG:3857"), icon);
+    let marker_e = new Tmap.Marker(new Tmap.LonLat(
+      "127.10331814639885", "37.403049076341794").transform("EPSG:4326", "EPSG:3857"), icon2);
     //설정한 좌표를 "EPSG:3857"로 좌표변환한 좌표값으로 설정합니다.
     markerLayer.addMarker(marker_e);//마커 레이어에 마커 추가
   
-    var headers = {}; 
+    let headers = {}; 
     headers["appKey"]="a53a0843-aa54-43c3-b8e0-e8202a107d2b";
     //실행을 위한 키 입니다. 발급받으신 AppKey를 입력하세요.
     $.ajax({
@@ -107,62 +110,73 @@ export default class TmapView extends React.Component {
       },
       //데이터 로드가 성공적으로 완료되었을 때 발생하는 함수입니다.
       success: function(response) {
-        var prtcl = response;
+        let prtcl = response;
         // 결과 출력
-        var innerHtml ="";
-        var prtclString = new XMLSerializer().serializeToString(prtcl);//xml to String	
-        var xmlDoc = $.parseXML( prtclString ),
+        let innerHtml ="";
+        let prtclString = new XMLSerializer().serializeToString(prtcl);//xml to String	
+        let xmlDoc = $.parseXML( prtclString ),
         $xml = $( xmlDoc ),
         $intRate = $xml.find("Document");
 
-        var tDistance = "총 거리 : " + 
+        let tDistance = "총 거리 : " + 
           ($intRate[0].getElementsByTagName("tmap:totalDistance")[0].
           childNodes[0].nodeValue/1000).toFixed(1)+"km,";
-        var tTime = " 총 시간 : " + 
+        let tTime = " 총 시간 : " + 
           ($intRate[0].getElementsByTagName("tmap:totalTime")[0].
-          childNodes[0].nodeValue/60).toFixed(0)+"분,";	
-        var tFare = " 총 요금 : " + 
-          $intRate[0].getElementsByTagName("tmap:totalFare")[0].childNodes[0].nodeValue+"원,";	
-        var taxiFare = " 예상 택시 요금 : " + 
-         $intRate[0].getElementsByTagName("tmap:taxiFare")[0].childNodes[0].nodeValue+"원";	
+          childNodes[0].nodeValue/60).toFixed(0)+"분,";
+        let tFare = " 총 요금 : " + 
+          $intRate[0].getElementsByTagName("tmap:totalFare")[0].childNodes[0].nodeValue+"원,";
+        let taxiFare = " 예상 택시 요금 : " + 
+          $intRate[0].getElementsByTagName("tmap:taxiFare")[0].childNodes[0].nodeValue+"원";
 
         $("#result").text(tDistance+tTime+tFare+taxiFare);
   
         routeLayer.removeAllFeatures();//레이어의 모든 도형을 지웁니다.
+
+        let places = $intRate[0].getElementsByTagName("Placemark");
+        let paths = [];
+        for(let i = 0; i < places.length; i++) {
+          let turnType = places[i].getElementsByTagName("tmap:turnType");
+          if(turnType.length > 0) turnType = turnType[0].childNodes[0].nodeValue;
+          else turnType = 11;
+          let placeDesc = places[i].getElementsByTagName("description")[0].childNodes[0].nodeValue;
+          paths.push(turnType + ":" + placeDesc);
+        }
+        console.log(paths);
   		
-        var traffic = $intRate[0].getElementsByTagName("traffic")[0];
+        let traffic = $intRate[0].getElementsByTagName("traffic")[0];
         //교통정보가 포함되어 있으면 교통정보를 포함한 경로를 그려주고
         //교통정보가 없다면  교통정보를 제외한 경로를 그려줍니다.
         if(!traffic){
           // 시작
-          var size = new Tmap.Size(24, 38);
+          let size = new Tmap.Size(24, 38);
           //아이콘 크기 설정
-          var offset = new Tmap.Pixel(-(size.w / 2), -size.h);
+          let offset = new Tmap.Pixel(-(size.w / 2), -size.h);
           //아이콘 중심점 설정
-          var icon = new Tmap.IconHtml(
+          let icon1 = new Tmap.IconHtml(
             "< img src='http://tmapapis.sktelecom.com/upload/tmap/marker/pin_r_m_s.png' / >", size, offset);
           //마커 아이콘 설정
           marker_s = new Tmap.Marker(new Tmap.LonLat(
-            "126.9850380932383", "37.566567545861645").transform("EPSG:4326", "EPSG:3857"), icon);
+            "126.9850380932383", "37.566567545861645").transform("EPSG:4326", "EPSG:3857"), icon1);
           //설정한 좌표를 "EPSG:3857"로 좌표변환한 좌표값으로 설정합니다.
           markerLayer.addMarker(marker_s);
           //마커 레이어에 마커 추가
   
           // 도착
-          var icon = new Tmap.IconHtml(
+          let icon2 = new Tmap.IconHtml(
             "< img src='http://tmapapis.sktelecom.com/upload/tmap/marker/pin_r_m_e.png' / >", size, offset);
           marker_e = new Tmap.Marker(new Tmap.LonLat(
-            "127.10331814639885", "37.403049076341794").transform("EPSG:4326", "EPSG:3857"), icon);
+            "127.10331814639885", "37.403049076341794").transform("EPSG:4326", "EPSG:3857"), icon2);
           markerLayer.addMarker(marker_e);
   
-          var prtclLine = new Tmap.Format.KML({extractStyles:true, extractAttributes:true}).read(prtcl);
+          let prtclLine = new Tmap.Format.KML({extractStyles:true, extractAttributes:true}).read(prtcl);
           //데이터(prtcl)를 읽고, 벡터 도형(feature) 목록을 리턴합니다.
   
           //표준 데이터 포맷인 KML을 Read/Write 하는 클래스 입니다.
           //벡터 도형(Feature)이 추가되기 직전에 이벤트가 발생합니다.
           routeLayer.events.register("beforefeatureadded", routeLayer, onBeforeFeatureAdded);
           function onBeforeFeatureAdded(e) {
-            var style = {};
+            let style = {};
             switch (e.feature.attributes.styleUrl) {
             case "#pointStyle":
               style.externalGraphic = "http://topopen.tmap.co.kr/imgs/point.png"; 
@@ -187,7 +201,7 @@ export default class TmapView extends React.Component {
           markerLayer.removeMarker(marker_s); 
           markerLayer.removeMarker(marker_e);
           routeLayer.removeAllFeatures();//레이어의 모든 도형을 지웁니다.
-          var trafficColors = {
+          let trafficColors = {
             extractStyles:true,
             /* 실제 교통정보가 표출되면 아래와 같은 Color로 Line이 생성됩니다. */
             trafficDefaultColor:"#000000", //Default
@@ -195,7 +209,7 @@ export default class TmapView extends React.Component {
             trafficType2Color:"#8E8111", //지체
             trafficType3Color:"#FF0000", //정체				
           };    
-          var kmlForm = new Tmap.Format.KML(trafficColors).readTraffic(prtcl);
+          let kmlForm = new Tmap.Format.KML(trafficColors).readTraffic(prtcl);
           routeLayer = new Tmap.Layer.Vector("vectorLayerID"); //백터 레이어 생성
           routeLayer.addFeatures(kmlForm); //교통정보를 백터 레이어에 추가   
       		
@@ -203,7 +217,8 @@ export default class TmapView extends React.Component {
         }
       	
         map.zoomToExtent(routeLayer.getDataExtent());
-        //map의 zoom을 routeLayer의 영역에 맞게 변경합니다.	
+        //map의 zoom을 routeLayer의 영역에 맞게 변경합니다.
+        this_.setState({paths: paths});
       },
       //요청 실패시 콘솔창에서 에러 내용을 확인할 수 있습니다.
       error:function(request,status,error){
@@ -237,6 +252,18 @@ export default class TmapView extends React.Component {
     );
   }
 
+  renderRow(row, index) {
+    let direction = row.split(":")[0];
+    let desc = row.split(":")[1];
+    return (
+      <ListItem key={"details-" + index}>
+        <div className="center">
+          {desc}
+        </div>
+      </ListItem>
+    );
+  }
+
   render() {
     return (
       <Page renderToolbar = {this.renderToolbar.bind(this)}>
@@ -249,6 +276,9 @@ export default class TmapView extends React.Component {
         <p id="result"></p>
         <div id="map_div">
         </div>
+        <List dataSource = {this.state.paths} 
+          renderRow={this.renderRow.bind(this)} 
+          renderHeader={() => <ListHeader>Details</ListHeader>}/>
       </Page>
     );
   } 
