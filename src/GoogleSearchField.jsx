bbff1ec361a013a1285a803ba26bd9dd2e97d6f5
Yoseob Kim <yo1226@gmail.com>
2018-10-19 09:42:19 +0000

    autocomplete test

--- src/GoogleSearchField.jsx
+++ src/GoogleSearchField.jsx
@@ -6,6 +6,9 @@ import {GoogleApiWrapper} from 'google-maps-react';
 export class GoogleSearchField extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      input: ""
+    };
   }
 
   componentDidMount() {
@@ -32,64 +35,81 @@ export class GoogleSearchField extends React.Component {
 
       this.map = new maps.Map(node, mapConfig);
       let input = document.getElementById('pac-input');
-      this.searchBox = new maps.places.SearchBox(input);
-      this.map.controls[maps.ControlPosition.TOP_LEFT];
+      this.searchBox = new maps.places.Autocomplete(input, {
+        componentRestrictions: {'country': 'kr'}
+      });
 
-      this.markers = [];
-      this.searchBox.addListener('places_changed', this.placeChanged.bind(this));
+      this.searchBox.bindTo('bounds', this.map);
+      this.searchBox.setTypes([]);
+      this.searchBox.setFields(
+        ['address_components', 'geometry', 'icon', 'name', 'types', 'formatted_address']);
+
+      this.infoWindow = new maps.InfoWindow();
+      this.infoWindowContent = document.getElementById('infowindow-content');
+      this.infoWindow.setContent(this.infoWindowContent);
+
+      this.marker = new maps.Marker({
+        map: this.map,
+        anchorPoint: new maps.Point(0, -29)
+      });
+
+      this.searchBox.addListener('place_changed', this.placeChanged.bind(this));
     }
   }
 
   placeChanged() {
-    let google = this.props.google;
-    let places = this.searchBox.getPlaces();
-    if(places.length == 0) {
+    this.infoWindow.close();
+    this.marker.setVisible(false);
+
+    let place = this.searchBox.getPlace();
+    console.log(place);
+    if(!place.geometry) {
       return;
     }
-    
-    this.markers.forEach(function(marker) {
-      marker.setMap(null);
-    });
-    this.markers = [];
 
-    let bounds = new google.maps.LatLngBounds();
-    let this_ = this;
+    if(!place.types.includes("lodging")) {
+      console.log("This is not an accomodation.");
+    }
+
+    if(place.geometry.viewport) {
+      this.map.fitBounds(place.geometry.viewport);
+    } else {
+      this.map.setCenter(place.geometry.location);
+      this.map.setZoom(17); // Why 17? Because it looks good.
+    }
+
+    this.marker.setPosition(place.geometry.location);
+    this.marker.setVisible(true);
+
+    let address = '';
+    if(place.address_components) {
+      address = [
+        (place.address_components[0] && place.address_components[0].short_name || ''),
+        (place.address_components[1] && place.address_components[1].short_name || ''),
+        (place.address_components[2] && place.address_components[2].short_name || '')
+      ].join(' ');
+    }
+  
+    document.getElementById('place-icon').src = place.icon;
+    document.getElementById('place-name').textContent = place.name;
+    document.getElementById('place-address').textContent = address;
+    this.infoWindow.open(this.map, this.marker);
+
     let ret = [];
-
-    places.forEach(function(place) {
-      if(!place.geometry) {
-        console.log("Returned place contains no geometry");
-        return;
-      }
-      var icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
-      this_.markers.push(new google.maps.Marker({
-        map: this_.map,
-        icon: icon,
-        title: place.name,
-        position: place.geometry.location
-      }));
-
-      ret.push({
-        name: place.name,
-        addr: place.formatted_address,
-        lat: place.geometry.location.lat(),
-        lng: place.geometry.location.lng()
-      });
-
-      if(place.geometry.viewport) {
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
+    ret.push({
+      name: place.name,
+      addr: place.formatted_address,
+      lat: place.geometry.location.lat(),
+      lng: place.geometry.location.lng()
     });
-    this.map.fitBounds(bounds);
+
     this.props.onSearchDone(ret);
+
+  }
+
+  onFocus(e) {
+    let input = document.getElementById('pac-input');
+    input.value='';
   }
 
   render() {
@@ -101,9 +121,15 @@ export class GoogleSearchField extends React.Component {
 
     return (
       <div style={{textAlign: "center"}}>
-        <input id="pac-input" type="text" placeholder="Search Box" 
-          style={{width: "95%", height: "30px"}} /> 
+        <input id="pac-input" type="text" 
+          style={{width: "95%", height: "30px"}} 
+          onFocus={this.onFocus.bind(this)}/> 
         <div ref="map" style={style}>
+        </div>
+        <div id="infowindow-content">
+          <img src="" style={{width: "16px", height: "16px"}} id="place-icon" />
+          <b id="place-name"></b><br />
+          <p id="place-address"></p>
         </div>
       </div>
     )
