d7f8a280cb2e8b7a009bac25cec4e59b1f894a0a
Yoseob Kim <yo1226@gmail.com>
2018-11-06 15:56:10 +0000

    clamp logic changed

--- src/HomePlanCard.jsx
+++ src/HomePlanCard.jsx
@@ -2,28 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Card} from 'react-onsenui';
 
-import './HomePageCSS';
+import LinesEllipsis from 'react-lines-ellipsis';
 
 import {HomePlanCardStyle} from './Styles';
 
 export default class HomePlanCard extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      overview: "가장 제주다운 풍경을 만날 수 있는 방법은 차편이나 걸어서 해안도로를 달려보는 것. 대자연의 너른 품에서 바름과 구름, 돌과 나무와 하늘을 길동무 삼아 달리며 심신의 여유와 자유로움을 마음껏 느껴보시길.",
+      title: "놀멍쉬멍 제주 바다 구경떠나기",
+      img: "https://tong.visitkorea.or.kr/cms/resource/08/197808_image2_1.jpg"
+    }
   }
 
   render() {
     const card = HomePlanCardStyle.card;
     return (
       <div className="card" style={card.style}>
-        <img src="https://tong.visitkorea.or.kr/cms/resource/08/197808_image2_1.jpg"
+        <img src={this.state.img}
           style={card.img.style} />
         <div className="card__title" style={card.title.style}>
-          <strong>놀멍쉬멍 제주 바다 구경떠나기</strong>
+          <strong>{this.state.title}</strong>
         </div>
         <div className="card__content" style={card.content.style}>
-          <div className="line-clamp">
-            가장 제주다운 풍경을 만날 수 있는 방법은 차편이나 걸어서 해안도로를 달려보는 것. 대자연의 너른 품에서 바름과 구름, 돌과 나무와 하늘을 길동무 삼아 달리며 심신의 여유와 자유로움을 마음껏 느껴보시길.
-          </div>
+          <LinesEllipsis 
+            text={this.state.overview} 
+            maxLine="2"
+            ellipsis="..."
+            trimRight
+            basedOn="letters" />
         </div>
       </div>
     );
